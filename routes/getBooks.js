d9d36e0651c9ec80a710fc3ae2da26e0b8ed5b89
aakash_raj <aakash.raj5@gmail.com>
2018-02-05 13:06:39 +0530

    Tested for server response which had JSON object grouped by authors

--- routes/getBooks.js
+++ routes/getBooks.js
@@ -1,10 +1,11 @@
 const https = require('https');
 
+let api1Data = {};
+
 module.exports = [{
   path: '/getBooks',
   method: 'GET',
   handler: (request, reply) => {
-    let api1Data = {};
     const promise = new Promise((resolve) => {
       https.get('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks', (res) => {
         res.on('data', (data) => {
@@ -14,16 +15,23 @@ module.exports = [{
       });
     });
     promise.then((data) => {
-      let flag = 1;
+      const flag = 1;
+      let count = 0;
       for (let i = 0; i < api1Data.books.length; i += 1) {
         https.get(data + api1Data.books[i].id, (response) => {
           response.on('data', (data2) => {
             api1Data.books[i].rating = JSON.parse(data2).rating;
             // console.log(api1Data.books[i]);
-            if (flag == 1) {
-              flag = 0;
-              // reply({ responseFromApi1: ap1Data.books[i] });
-              reply({ message: api1Data.books[i] });
+            count++;
+            if (count === api1Data.books.length - 1) {
+              const arr = Array.from(api1Data.books).reduce((groups, item) => {
+                const val = item.Author;
+                groups[val] = groups[val] || [];
+                groups[val].push(item);
+                return groups;
+              }, {});
+              reply(arr);
+              console.log(arr);
             }
           });
         });
--- src/server.test.js
+++ src/server.test.js
@@ -23,14 +23,25 @@ describe('Testing server', () => {
   //     // Models.users.destroy({ truncate: true });
   //   });
   // });
-  test('Testing API 2 call', () => {
+  test('Testing API 2 call Expected output: JSON with rating included', () => {
     const options = {
       method: 'GET',
       url: '/getBooks',
     };
     Server.inject(options, (response) => {
-      console.log(response.result.message.rating);
-      expect(response.result.message.rating).not.toBe(undefined);
+      // console.log(response.result.message.rating);
+      expect(response.result['Sidney Sheldon'][0].rating).not.toBe(undefined);
+      // Models.users.destroy({ truncate: true });
+    });
+  });
+  test('Testing Json output, Expected : Object grouped by author names with rating', () => {
+    const options = {
+      method: 'GET',
+      url: '/getBooks',
+    };
+    Server.inject(options, (response) => {
+      // console.log(response.result['Sidney Sheldon']);
+      expect(response.result['Sidney Sheldon']).not.toBe(undefined);
       // Models.users.destroy({ truncate: true });
     });
   });
