d3a746ec9d0e9defd379a4b8d3d86f6e89b6d958
aakash_raj <aakash.raj5@gmail.com>
2018-02-16 06:09:01 +0530

    Refactored and modularizes code for grouping books by author

--- routes/getBooks.js
+++ routes/getBooks.js
@@ -1,42 +1,64 @@
 const https = require('https');
 const Models = require('../models');
 
-let api1Data = {};
+
+const getAllBooksData = () => {
+  let api1Data = {};
+  const promise = new Promise((resolve) => {
+    https.get('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks', (res) => {
+      res.on('data', (data) => {
+        api1Data = JSON.parse(data);
+        resolve(api1Data);
+      });
+    });
+  });
+  return promise;
+};
+
+
+const booksGroupedByAuthor = api1Data => Array.from(api1Data.books).reduce((groups, item) => {
+  const val = item.Author;
+  groups[val] = groups[val] || [];
+  groups[val].push(item);
+  return groups;
+}, {});
+
+
+let getBookRatings = (api1Data) => {
+  let promiseArray = api1Data.books.map((book)=>{
+    return new Promise((resolve) => {
+      https.get(`https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findBookById/${book.id}`, (response) => {
+        response.on('data', (data2) => {
+          resolve(JSON.parse(data2).rating);
+        });
+      });
+    })
+  });
+  return promiseArray;
+}
+
+
+let mergeRatings = (api1Data, ratingsArray) => {
+  for(let i=0;i<api1Data.books.length;i++)
+  {
+    api1Data.books[i].ratings = ratingsArray[i];
+  }
+  return api1Data;
+}
+
 
 module.exports = [{
   path: '/getBooks',
   method: 'GET',
   handler: (request, reply) => {
-    const promise = new Promise((resolve) => {
-      https.get('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks', (res) => {
-        res.on('data', (data) => {
-          api1Data = JSON.parse(data);
-          resolve('https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findBookById/');
-        });
+    const promise = getAllBooksData();
+    promise.then((api1Data) => {
+      let promiseArray = getBookRatings(api1Data);
+      Promise.all(promiseArray).then((ratingsArray) => {
+        let allBookswithRatings = mergeRatings(api1Data,ratingsArray);
+        let allBooksByAuthor = booksGroupedByAuthor(allBookswithRatings);
+        reply (allBooksByAuthor);
       });
-    });
-    promise.then((data) => {
-      const flag = 1;
-      let count = 0;
-      for (let i = 0; i < api1Data.books.length; i += 1) {
-        https.get(data + api1Data.books[i].id, (response) => {
-          response.on('data', (data2) => {
-            api1Data.books[i].rating = JSON.parse(data2).rating;
-            // console.log(api1Data.books[i]);
-            count++;
-            if (count === api1Data.books.length - 1) {
-              const arr = Array.from(api1Data.books).reduce((groups, item) => {
-                const val = item.Author;
-                groups[val] = groups[val] || [];
-                groups[val].push(item);
-                return groups;
-              }, {});
-              reply(arr);
-              console.log(arr);
-            }
-          });
-        });
-      }
     });
   },
 },
@@ -50,7 +72,6 @@ module.exports = [{
         bookid: element.id,
         name: element.Name,
         rating: element.rating,
-        like: 0,
       });
     });
   },
